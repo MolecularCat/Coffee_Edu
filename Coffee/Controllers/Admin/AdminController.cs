ab94200fbb3dbbd309869fdfd402ccaebdfd16f8
MolecularCat <geb@inbox.ru>
2023-10-24 16:36:13 +0300

    Add Admin Controller and View. Add Roles in Program.cs. Change Razor builder to Controller and View builder in Program.cs

---
+++ Coffee/Controllers/Admin/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.Controllers.Admin
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
