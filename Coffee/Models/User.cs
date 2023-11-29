d70df6f3dad8fef2305da1194aefa705c3db617d
MolecularCat <geb@inbox.ru>
2023-11-29 19:10:25 +0300

    Corrected fields Models/User.cs Add Lockout end date and href for block and unblock user in Views/Admin/Users.cshtml

--- Coffee/Models/User.cs
+++ Coffee/Models/User.cs
@@ -5,7 +5,5 @@ namespace Coffee.Models
     public class User : IdentityUser
     {
         public DateTime Created {  get; set; }
-        public string UserName {  get; set; }
-        public string Email { get; set; }
     }
 }
